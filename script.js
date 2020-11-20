cb4cd3be92d3409fc51d5849da69c1cff9691f20
TehRamsus <50022415+TehRamsus@users.noreply.github.com>
2020-11-20 17:46:19 +0200

    added loading logo and new page

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-function Musiikki(link){
-    var video = document.getElementById('video');
-    var videoSrc = link;
-    //
-    // First check for native browser HLS support
-    //
-    if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = videoSrc;
-      video.addEventListener('loadedmetadata', function() {
-        video.play();
-      });
-    //
-    // If no native HLS support, check if hls.js is supported
-    //
-    } else if (Hls.isSupported()) {
-      var hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED, function() {
-        video.play();
-      });
-    }
-}
-
-
-
-
+$(document).ready(function () {
+  $(window).load(function(){
+  $('.loader-wrapper').delay(2000).fadeOut();
+  });
+});
